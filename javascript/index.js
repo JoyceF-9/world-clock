369417107a93f962b6447cd6f8443b5866dcda6e
JoyceF-9 <118493335+JoyceF-9@users.noreply.github.com>
2023-06-10 22:01:54 -0400

    Added current Time Zone

--- javascript/index.js
+++ javascript/index.js
@@ -41,21 +41,23 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  let cityName = CityTImeZone.replace("_", " ").split("/")[1];
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("cities");
+  let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
-<div class="city">
-          <div>
-            <h2>${cityTimeZone}</h2>
-            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-          </div>
-          <div class="time">${cityTime.format(
-            "h:mm:ss"
-          )} <small>${cityTime.format("A")}</small>
-          </div>
-        </div>
-`;
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
 }
 
 updateTime();
